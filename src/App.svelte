9ca5a3387a922a7f79aefe67d033ba175889e0fc
Emma Davenport <davenport.physics@gmail.com>
2023-10-27 13:32:51 -0500

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  
+  import { Router } from '@roxi/routify'
+  import { routes } from "../.routify/routes";
+
+</script>
+
+<main>
+  <Router {routes} />
+</main>
+
+<style>
+</style>
---
+++ src/pages/index.svelte
@@ -0,0 +1,13 @@
+
+<script lang="ts">
+    
+</script>
+
+<div class="container">
+    <div class="flex flex-row items-center justify-center gap-1">
+        <input type="text" placeholder="Path" class="rounded-md border px-1 w-96 outline-indigo-500"/>
+        <button type="button" class="rounded border px-2 hover:bg-slate-100">Conan Exiles Path</button>
+    </div>
+</div>
+
+<style></style>
